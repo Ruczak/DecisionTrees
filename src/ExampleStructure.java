6efa0e48e29fedb57f3da621e95c4180aeb4290e
Sarnowski, Igor (Stud. DACS) <i.sarnowski@student.maastrichtuniversity.nl>
2024-12-23 15:54:44 +0100

    Amended example usage

--- src/ExampleStructure.java
+++ src/ExampleStructure.java
@@ -1,9 +1,9 @@
 public class ExampleStructure {
     private int id;
     private double value1;
-    private double value2;
+    private int value2;
 
-    public ExampleStructure(int id, double value1, double value2) {
+    public ExampleStructure(int id, double value1, int value2) {
         this.id = id;
         this.value1 = value1;
         this.value2 = value2;
@@ -27,12 +27,12 @@ public class ExampleStructure {
 
     public static void main(String[] args) {
         ExampleStructure[] exampleStructures = {
-                new ExampleStructure(1, 3.0, 90.5),
-                new ExampleStructure(2, -4.5, 60.2),
-                new ExampleStructure(3, 1.3, 30.1),
-                new ExampleStructure(4, 2.4, 80.1),
-                new ExampleStructure(5, -3, 39.2),
-                new ExampleStructure(6, 1.8, 107.3),
+                new ExampleStructure(1, 3.0, 90),
+                new ExampleStructure(2, -4.5, 60),
+                new ExampleStructure(3, 1.3, 30),
+                new ExampleStructure(4, 2.4, 80),
+                new ExampleStructure(5, -3, 39),
+                new ExampleStructure(6, 1.8, 107),
         };
 
         DecisionTree.Tester<ExampleStructure, Double> value1Tester = new DecisionTree.Tester<ExampleStructure, Double>() {
@@ -41,14 +41,14 @@ public class ExampleStructure {
                 return object.getValue1() >= splitValue;
             }
         };
-        DecisionTree.Tester<ExampleStructure, Double> value2Tester = new DecisionTree.Tester<ExampleStructure, Double>() {
+        DecisionTree.Tester<ExampleStructure, Integer> value2Tester = new DecisionTree.Tester<ExampleStructure, Integer>() {
             @Override
-            public boolean test(ExampleStructure object, Double splitValue) {
+            public boolean test(ExampleStructure object, Integer splitValue) {
                 return object.getValue2() >= splitValue;
             }
         };
 
-        DecisionTree<ExampleStructure, Double> value2Subtree = new DecisionTree<>(60.0, value2Tester);
+        DecisionTree<ExampleStructure, Integer> value2Subtree = new DecisionTree<>(60, value2Tester);
         DecisionTree<ExampleStructure, Double> value1Subtree = new DecisionTree<>(0.0, value1Tester, null, value2Subtree);
 
         for (ExampleStructure exampleStructure : exampleStructures) {
